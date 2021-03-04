106d0d6babd6ea20bc6f59a638847a2e2964be97
Alexandre Maranhão <alexandremr01@gmail.com>
2021-03-03 21:10:59 -0300

    second commit

--- data/game.py
+++ data/game.py
@@ -1,4 +1,9 @@
 class Game:
     def __init()__:
         self.running = True
-        
+
+    def main():
+        while not self.done:
+            lag += game.clock.tick(self.fps)
+            self.event_loop()
+            self.draw(lag/TIME_PER_UPDATE)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame as pg
 from data.main import main
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.main()
     pg.quit()
     sys.exit()
