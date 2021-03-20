e4338caeeb7894e368861412c34084a494881b76
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-03-20 20:33:57 -0300

    rendering working now

--- data/map/tile.py
+++ data/map/tile.py
@@ -1,9 +1,24 @@
 from ..setup import graphics_dict
+from ..components.base.sprite import Sprite
+
+
+class Tile:
+    """
+    Individual tile with name, sprite and walkable flag.
+    """
+    def __init__(self, name, sprite, walkable=True):
+        self.name = name
+        self.sprite = sprite
+        self.walkable = walkable
 
 
 class Tiles:
+    """
+    Class that holds all tiles that could be rendered by the map.
+    """
     def __init__(self):
-        self.terrain = [graphics_dict["map"].get_image(0),
-                        graphics_dict["map"].get_image(11),
-                        graphics_dict["map"].get_image(49)]
+        self.terrain = {0: Tile("DIRT", Sprite((0, 0), graphics_dict["map"].get_image(49, (50, 50)))),
+                        1: Tile("GRASS", Sprite((0, 0), graphics_dict["map"].get_image(0, (50, 50)))),
+                        2: Tile("CHECKERED", Sprite((0, 0), graphics_dict["map"].get_image(11, (50, 50))))}
         self.structures = []
+
