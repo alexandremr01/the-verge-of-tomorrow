9f850a88115dbbc6fca0c94ee0838faffedd0429
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-04-09 01:52:52 -0300

    Divided structure generation throughout frames. Now, noise parameters need to be retuned

--- data/map/chunk.py
+++ data/map/chunk.py
@@ -8,158 +8,211 @@ class Chunk:
         self.position = position
         self.topleft = position * CHUNK_SIZE - CHUNK_ARRAY / 2
         self.tilegrid = None
+        self.structuregrid = None
+        self.structures = None
+        self.seed = np.random.randint(0, 10000)
         self.surface = None
         self.is_rendering = True
-        self.render_step = 0
+        self.terrain_step = 0
+        self.terrain_steps = RENDER_STEPS
+        self.structures_step = -1
+        self.structures_steps = 0
+        self.draw_step = 0
+        self.draw_steps = RENDER_STEPS
 
     def is_rendered(self):
         return self.tilegrid is not None
 
     def de_render(self):
         self.tilegrid = None
+        self.structuregrid = None
         self.surface = None
+        self.structures = None
+        self.structures_step = -1
+        self.terrain_step = self.draw_step = 0
 
-    def render(self, generator, tiles):
-        self.is_rendering = True
-        start_position = self.topleft + np.array([0, CHUNK_SIZE // RENDER_STEPS * self.render_step])
-        new_load = np.array([[generator.noise2d((start_position[0] / TILE_SIZE + j) / 3,
-                                                -(start_position[1] / TILE_SIZE + i) / 3) + 1.0
+    def generate_structure_variables(self, number_of_structures):
+        self.structuregrid = np.zeros((CHUNK_TILE_RATIO, CHUNK_TILE_RATIO))
+        positions = np.array([[0.25, 0.25], [0.75, 0.75], [0.25, 0.75], [0.75, 0.25]]) * CHUNK_TILE_RATIO
+        positions = np.concatenate((positions.astype(int), [[1], [2], [3], [4]]), axis=1)
+        np.random.seed(self.seed)
+        positions = np.take(positions,
+                            np.random.choice([0, 1, 2, 3], size=number_of_structures, replace=False),
+                            axis=0)
+        self.structures = {}
+        for position in positions:
+            structure_seed = self.seed + position[2]
+            np.random.seed(structure_seed)
+            number_of_directions = np.random.choice([1, 2, 3, 4], p=[0.35, 0.35, 0.2, 0.1])
+            np.random.seed(structure_seed)
+            directions = np.take([[1, 1, 11], [-1, 1, 12], [-1, -1, 13], [1, -1, 14]],
+                                 np.random.choice(np.array([0, 1, 2, 3]), size=number_of_directions,
+                                                  replace=False), axis=0)
+            generation_variables = []
+            for direction in directions:
+                direction_seed = structure_seed + direction[2]
+                np.random.seed(direction_seed)
+                width = np.random.randint(6, CHUNK_TILE_RATIO / 4 - 1)
+                np.random.seed(direction_seed + 5)
+                height = np.random.randint(6, CHUNK_TILE_RATIO / 4 - 1)
+                generation_variables.append([direction[:2], [width, height]])
+            self.structures[tuple(position[:2])] = generation_variables
+
+    def generate_terrain(self, generator):
+        start_position = self.topleft + np.array([0, CHUNK_SIZE // self.terrain_steps * self.terrain_step])
+        new_load = np.array([[generator.noise2d((start_position[0] / TILE_SIZE + j) / 2,
+                                                -(start_position[1] / TILE_SIZE + i) / 2) / 2 + 0.5
                               for j in range(CHUNK_TILE_RATIO)]
                              for i in range(CHUNK_TILE_RATIO_STEPS)])
-        if self.render_step == 0:
+        if self.terrain_step == 0:
             self.tilegrid = new_load
             self.surface = pygame.Surface(CHUNK_ARRAY)
         else:
             self.tilegrid = np.concatenate((self.tilegrid, new_load), axis=0)
-        self.decode_and_draw(CHUNK_TILE_RATIO_STEPS * self.render_step, tiles)
-        self.render_step += 1
-        if self.render_step is RENDER_STEPS:
-            self.gen_structures(int(100000 * (generator.noise2d(self.topleft[0], self.topleft[1]) + 1)), tiles)
-            self.is_rendering = False
-            self.render_step = 0
 
-    def blit(self, tiles, x, y):
-        self.surface.blit(tiles[self.tilegrid[x][y]].sprite.get_image(),
-                          np.array([x, y]) * TILE_SIZE)
+    def check_wall_collision(self, x, y, tiles):
+        if tiles.is_what(self.structuregrid[x + 1][y], "FLOOR"):
+            if tiles.is_what(self.structuregrid[x][y + 1], "FLOOR"):
+                self.structuregrid[x][y] = tiles.code["WALL_BOTTOM_RIGHT"]
+            else:
+                self.structuregrid[x][y] = tiles.code["WALL_TOP_RIGHT"]
+        else:
+            if tiles.is_what(self.structuregrid[x][y + 1], "FLOOR"):
+                self.structuregrid[x][y] = tiles.code["WALL_BOTTOM_LEFT"]
+            else:
+                self.structuregrid[x][y] = tiles.code["WALL_TOP_LEFT"]
 
-    def gen_structures(self, seed, tiles):
-        np.random.seed(seed)
-        if np.random.rand() < 1:
-            np.random.seed(seed)
-            number_of_generators = np.random.choice([1, 2, 3, 4], p=[0.5, 0.3, 0.15, 0.05])
-            positions = (np.array([[0.25, 0.25], [0.75, 0.75], [0.25, 0.75], [0.75, 0.25]]) * CHUNK_TILE_RATIO)
-            positions = np.concatenate((positions.astype(int), np.array([[1], [2], [3], [4]])), axis=1)
-            np.random.seed(seed)
-            positions = np.take(positions,
-                                np.random.choice(np.array([0, 1, 2, 3]), size=number_of_generators, replace=False),
-                                axis=0)
+    def generate_structure(self, tiles):
+        position = 0
+        for s in enumerate(self.structures):
+            if s[0] == self.structures_step:
+                position = s[1]
+                break
 
-            for position in positions:
-                position_seed = seed + position[2]
-                np.random.seed(position_seed)
-                number_of_directions = np.random.choice([1, 2, 3, 4], p=[0.35, 0.35, 0.2, 0.1])
-                np.random.seed(position_seed)
-                directions = np.take(np.array([[1, 1, 11], [-1, 1, 12], [-1, -1, 13], [1, -1, 14]]),
-                                     np.random.choice(np.array([0, 1, 2, 3]), size=number_of_directions, replace=False),
-                                     axis=0)
+        for direction, length in self.structures[position]:
+            x_direction, y_direction = direction
+            width, height = length
 
-                for direction in directions:
-                    direction_seed = position_seed + direction[2]
-                    np.random.seed(direction_seed)
-                    x = np.random.randint(6, CHUNK_TILE_RATIO / 4 - 1)
-                    np.random.seed(direction_seed + 1)
-                    y = np.random.randint(6, CHUNK_TILE_RATIO / 4 - 1)
-                    for i in range(-1, x):
-                        for j in range(-1, y):
-                            x_entry = position[0] + direction[0] * i
-                            y_entry = position[1] + direction[1] * j
-                            self.tilegrid[x_entry][y_entry] = -(self.tilegrid[x_entry][y_entry] % 3) - 1
-                            self.blit(tiles.structures, x_entry, y_entry)
+            # Set floor
+            for i in range(-1, width):
+                for j in range(-1, height):
+                    x = position[0] + x_direction * i
+                    y = position[1] + y_direction * j
+                    self.structuregrid[x][y] = tiles.code["CHECKERED_BASIC_1"]
 
-                    # Draw left and right walls
-                    for i in [-2, x]:
-                        for j in range(-1, y):
-                            x_entry = position[0] + direction[0] * i
-                            y_entry = position[1] + direction[1] * j
-                            if not (-6 <= self.tilegrid[x_entry][y_entry] <= -1) and not self.tilegrid[x_entry][y_entry] == -8:
-                                self.tilegrid[x_entry][y_entry] = -7
-                                self.blit(tiles.structures, x_entry, y_entry)
-                            elif self.tilegrid[x_entry][y_entry] == -8:
-                                if -6 <= self.tilegrid[x_entry + 1][y_entry] <= -1:
-                                    if -6 <= self.tilegrid[x_entry][y_entry + 1] <= -1:
-                                        self.tilegrid[x_entry][y_entry] = -9  # BOTTOM RIGHT
-                                    else:
-                                        self.tilegrid[x_entry][y_entry] = -10  # TOP RIGHT
-                                else:
-                                    if -6 <= self.tilegrid[x_entry][y_entry + 1] <= -1:
-                                        self.tilegrid[x_entry][y_entry] = -11  # BOTTOM LEFT
-                                    else:
-                                        self.tilegrid[x_entry][y_entry] = -12  # TOP LEFT
-                                self.blit(tiles.structures, x_entry, y_entry)
+            # Set left and right walls
+            for i in [-2, width]:
+                for j in range(-1, height):
+                    x = position[0] + x_direction * i
+                    y = position[1] + y_direction * j
+                    if self.structuregrid[x][y] == tiles.code["WALL_TOP_BOTTOM"]:
+                        self.check_wall_collision(x, y, tiles)
+                    elif not tiles.is_what(self.structuregrid[x][y], "FLOOR"):
+                        self.structuregrid[x][y] = tiles.code["WALL_LEFT_RIGHT"]
 
-                    # Draw top and bottom walls
-                    for j in [-2, y]:
-                        for i in range(-1, x):
-                            x_entry = position[0] + direction[0] * i
-                            y_entry = position[1] + direction[1] * j
-                            if not (-6 <= self.tilegrid[x_entry][y_entry] <= -1) and not self.tilegrid[x_entry][y_entry] == -7:
-                                self.tilegrid[x_entry][y_entry] = -8
-                                self.blit(tiles.structures, x_entry, y_entry)
-                            elif self.tilegrid[x_entry][y_entry] == -7:
-                                if -6 <= self.tilegrid[x_entry + 1][y_entry] <= -1:
-                                    if -6 <= self.tilegrid[x_entry][y_entry + 1] <= -1:
-                                        self.tilegrid[x_entry][y_entry] = -9  # BOTTOM RIGHT
-                                    else:
-                                        self.tilegrid[x_entry][y_entry] = -10  # TOP RIGHT
-                                else:
-                                    if -6 <= self.tilegrid[x_entry][y_entry + 1] <= -1:
-                                        self.tilegrid[x_entry][y_entry] = -11  # BOTTOM LEFT
-                                    else:
-                                        self.tilegrid[x_entry][y_entry] = -12  # TOP LEFT
-                                self.blit(tiles.structures, x_entry, y_entry)
+            # Set top and bottom walls
+            for j in [-2, height]:
+                for i in range(-1, width):
+                    x = position[0] + x_direction * i
+                    y = position[1] + y_direction * j
+                    if self.structuregrid[x][y] == tiles.code["WALL_LEFT_RIGHT"]:
+                        self.check_wall_collision(x, y, tiles)
+                    elif not tiles.is_what(self.structuregrid[x][y], "FLOOR"):
+                        self.structuregrid[x][y] = tiles.code["WALL_TOP_BOTTOM"]
 
-                    # Draw Corners
-                    for i in [-2, x]:
-                        for j in [-2, y]:
-                            x_entry = position[0] + direction[0] * i
-                            y_entry = position[1] + direction[1] * j
-                            if self.tilegrid[x_entry][y_entry] >= 0:
-                                if self.tilegrid[x_entry + 1][y_entry] < 0:
-                                    if self.tilegrid[x_entry][y_entry + 1] < 0:
-                                        self.tilegrid[x_entry][y_entry] = -12  # TOP LEFT
-                                    else:
-                                        self.tilegrid[x_entry][y_entry] = -11  # BOTTOM LEFT
-                                else:
-                                    if self.tilegrid[x_entry][y_entry + 1] < 0:
-                                        self.tilegrid[x_entry][y_entry] = -10  # TOP RIGHT
-                                    else:
-                                        self.tilegrid[x_entry][y_entry] = -9  # BOTTOM RIGHT
-                                self.blit(tiles.structures, x_entry, y_entry)
+            # Set Corners
+            for i in [-2, width]:
+                for j in [-2, height]:
+                    x = position[0] + x_direction * i
+                    y = position[1] + y_direction * j
+                    if tiles.is_what(self.structuregrid[x][y], "TERRAIN"):
+                        if tiles.is_what(self.structuregrid[x + 1][y], "WALL"):
+                            if tiles.is_what(self.structuregrid[x][y + 1], "WALL"):
+                                self.structuregrid[x][y] = tiles.code["WALL_TOP_LEFT"]
+                            else:
+                                self.structuregrid[x][y] = tiles.code["WALL_BOTTOM_LEFT"]
+                        else:
+                            if tiles.is_what(self.structuregrid[x][y + 1], "WALL"):
+                                self.structuregrid[x][y] = tiles.code["WALL_TOP_RIGHT"]
+                            else:
+                                self.structuregrid[x][y] = tiles.code["WALL_BOTTOM_RIGHT"]
 
+    def render(self, generator, tiles):
+        self.is_rendering = True
+        if self.structures_step == -1:
+            np.random.seed(self.seed)
+            number_of_structures = int(np.random.choice([0, 1, 2, 3, 4], p=[0, 0.5, 0.3, 0.15, 0.05]))
+            if number_of_structures != 0:
+                self.generate_structure_variables(number_of_structures)
+            self.structures_steps = number_of_structures
+            self.structures_step += 1
+        else:
+            if self.terrain_step is not self.terrain_steps:
+                self.generate_terrain(generator)
+                self.terrain_step += 1
+            else:
+                if self.structures_step is not self.structures_steps:
+                    self.generate_structure(tiles)
+                    self.structures_step += 1
+                else:
+                    if self.draw_step is not self.draw_steps:
+                        self.draw(tiles)
+                        self.draw_step += 1
+                    else:
+                        self.is_rendering = False
 
-    def decode_and_draw(self, row, tiles):
+    def draw(self, tiles):
+        row = CHUNK_TILE_RATIO_STEPS * self.draw_step
         for i in range(CHUNK_TILE_RATIO_STEPS):
             for j in range(CHUNK_TILE_RATIO):
-                terrainnoise = self.tilegrid[row + i][j]
-                if 0.15 <= terrainnoise < 0.195:
-                    self.tilegrid[row + i][j] = 7
-                else:
-                    if 0.3 <= terrainnoise < 0.7:
-                        if 0.3 <= terrainnoise < 0.39:
-                            self.tilegrid[row + i][j] = 1
-                        elif 0.39 <= terrainnoise < 0.45:
-                            self.tilegrid[row + i][j] = 2
-                        elif 0.45 <= terrainnoise < 0.53:
-                            self.tilegrid[row + i][j] = 3
-                        elif 0.53 <= terrainnoise < 0.61:
-                            self.tilegrid[row + i][j] = 4
-                        elif 0.61 <= terrainnoise < 0.67:
-                            self.tilegrid[row + i][j] = 5
-                        else:
-                            self.tilegrid[row + i][j] = 6
-                    else:
-                        self.tilegrid[row + i][j] = 0
+                self.decode(row + i, j, tiles)
+                self.surface.blit(tiles.sprites[self.tilegrid[row + i][j]].sprite.get_image(),
+                                  np.array([row + i, j]) * TILE_SIZE)
 
-                self.blit(tiles.terrain, row + i, j)
+    def decode(self, i, j, tiles):
+        if self.structuregrid[i][j] == 0:
+            terrainnoise = self.tilegrid[i][j]
+            if 0.15 <= terrainnoise < 0.195:
+                self.tilegrid[i][j] = tiles.code["ROCK"]
+            else:
+                if 0.3 <= terrainnoise < 0.7:
+                    if 0.3 <= terrainnoise < 0.39:
+                        self.tilegrid[i][j] = tiles.code["GRASS_DARKLEAFS_1"]
+                    elif 0.39 <= terrainnoise < 0.45:
+                        self.tilegrid[i][j] = tiles.code["GRASS_DARKLEAFS_2"]
+                    elif 0.45 <= terrainnoise < 0.53:
+                        self.tilegrid[i][j] = tiles.code["GRASS_BRIGHTLEAFS_1"]
+                    elif 0.53 <= terrainnoise < 0.61:
+                        self.tilegrid[i][j] = tiles.code["GRASS_BRIGHTLEAFS_2"]
+                    elif 0.61 <= terrainnoise < 0.67:
+                        self.tilegrid[i][j] = tiles.code["GRASS_BRIGHTLEAFS_3"]
+                    else:
+                        self.tilegrid[i][j] = tiles.code["GRASS_BRIGHTLEAFS_4"]
+                else:
+                    self.tilegrid[i][j] = tiles.code["GRASS_PLAIN"]
+        elif tiles.is_what(self.structuregrid[i][j], "FLOOR"):
+            floornoise = self.tilegrid[i][j]
+            if 0.3 <= floornoise < 0.7:
+                if 0.3 <= floornoise < 0.45:
+                    self.tilegrid[i][j] = tiles.code["CHECKERED_BASIC_2"]
+                if 0.45 <= floornoise < 0.55:
+                    if 0.45 <= floornoise < 0.48:
+                        self.tilegrid[i][j] = tiles.code["CHECKERED_GRASS_1"]
+                    if 0.48 <= floornoise < 0.51:
+                        self.tilegrid[i][j] = tiles.code["CHECKERED_GRASS_2"]
+                    if 0.51 <= floornoise < 0.52:
+                        if 0.51 <= floornoise < 0.515:
+                            self.tilegrid[i][j] = tiles.code["GRASS_BRIGHTLEAFS_1"]
+                        else:
+                            self.tilegrid[i][j] = tiles.code["GRASS_BRIGHTLEAFS_2"]
+                    else:
+                        self.tilegrid[i][j] = tiles.code["CHECKERED_GRASS_3"]
+                else:
+                    self.tilegrid[i][j] = tiles.code["CHECKERED_BASIC_3"]
+            else:
+                self.tilegrid[i][j] = tiles.code["CHECKERED_BASIC_1"]
+        else:
+            self.tilegrid[i][j] = self.structuregrid[i][j]
+
 
 
--- data/map/tile.py
+++ data/map/tile.py
@@ -1,13 +1,13 @@
 from ..setup import graphics_dict
 from ..components.base.sprite import Sprite
 
+
 class Tile:
     """
     Individual tile with name, sprite and walkable flag.
     """
 
-    def __init__(self, name, sprite, walkable=True):
-        self.name = name
+    def __init__(self, sprite, walkable=True):
         self.sprite = sprite
         self.walkable = walkable
 
@@ -18,47 +18,63 @@ class Tiles:
     """
 
     def __init__(self):
-        self.terrain = {0: Tile("GRASS_PLAIN",
-                                Sprite((0, 0), graphics_dict["map"].get_image(0, (50, 50)))),
-                        1: Tile("GRASS_DARKLEAFS_1",
-                                Sprite((0, 0), graphics_dict["map"].get_image(16, (50, 50)))),
-                        2: Tile("GRASS_DARKLEAFS_2",
-                                Sprite((0, 0), graphics_dict["map"].get_image(24, (50, 50)))),
-                        3: Tile("GRASS_BRIGHTLEAFS_1",
-                                Sprite((0, 0), graphics_dict["map"].get_image(15, (50, 50)))),
-                        4: Tile("GRASS_BRIGHTLEAFS_2",
-                                Sprite((0, 0), graphics_dict["map"].get_image(31, (50, 50)))),
-                        5: Tile("GRASS_BRIGHTLEAFS_3",
-                                Sprite((0, 0), graphics_dict["map"].get_image(23, (50, 50)))),
-                        6: Tile("GRASS_BRIGHTLEAFS_4",
-                                Sprite((0, 0), graphics_dict["map"].get_image(32, (50, 50)))),
-                        7: Tile("ROCK",
-                                Sprite((0, 0), graphics_dict["map"].get_image(13, (50, 50))), False)}
-        self.structures = {-1: Tile("CHECKERED_BASIC_1",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(11, (50, 50)))),
-                           -2: Tile("CHECKERED_BASIC_2",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(4, (50, 50)))),
-                           -3: Tile("CHECKERED_BASIC_3",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(12, (50, 50)))),
-                           -4: Tile("CHECKERED_GRASS_1",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(5, (50, 50)))),
-                           -5: Tile("CHECKERED_GRASS_2",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(6, (50, 50)))),
-                           -6: Tile("CHECKERED_GRASS_3",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(7, (50, 50)))),
-                           -7: Tile("WALL_LEFT_RIGHT",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(25, (50, 50))), False),
-                           -8: Tile("WALL_TOP_BOTTOM",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(40, (50, 50))), False),
-                           -9: Tile("WALL_BOTTOM_RIGHT",
-                                    Sprite((0, 0), graphics_dict["map"].get_image(33, (50, 50)), None, (True, False)), False),
-                           -10: Tile("WALL_TOP_RIGHT",
-                                     Sprite((0, 0), graphics_dict["map"].get_image(36, (50, 50))), False),
-                           -11: Tile("WALL_BOTTOM_LEFT",
-                                     Sprite((0, 0), graphics_dict["map"].get_image(33, (50, 50))), False),
-                           -12: Tile("WALL_TOP_LEFT",
-                                     Sprite((0, 0), graphics_dict["map"].get_image(36, (50, 50)), None, (True, False)), False),
-                           -13: Tile("WALL_PILLAR",
-                                     Sprite((0, 0), graphics_dict["map"].get_image(0, (50, 50))), False),
-                           -14: Tile("WALL_BROKEN",
-                                     Sprite((0, 0), graphics_dict["map"].get_image(0, (50, 50))))}
+        self.code = {"GRASS_PLAIN": 1,
+                     "GRASS_DARKLEAFS_1": 2,
+                     "GRASS_DARKLEAFS_2": 3,
+                     "GRASS_BRIGHTLEAFS_1": 4,
+                     "GRASS_BRIGHTLEAFS_2": 5,
+                     "GRASS_BRIGHTLEAFS_3": 6,
+                     "GRASS_BRIGHTLEAFS_4": 7,
+                     "ROCK": 8,
+                     "CHECKERED_BASIC_1": 9,
+                     "CHECKERED_BASIC_2": 10,
+                     "CHECKERED_BASIC_3": 11,
+                     "CHECKERED_GRASS_1": 12,
+                     "CHECKERED_GRASS_2": 13,
+                     "CHECKERED_GRASS_3": 14,
+                     "WALL_LEFT_RIGHT": 15,
+                     "WALL_TOP_BOTTOM": 16,
+                     "WALL_BOTTOM_RIGHT": 17,
+                     "WALL_TOP_RIGHT": 18,
+                     "WALL_BOTTOM_LEFT": 19,
+                     "WALL_TOP_LEFT": 20,
+                     "WALL_PILLAR": 21,
+                     "WALL_BROKEN": 22}
+
+        self.sprites = {1: Tile(Sprite((0, 0), graphics_dict["map"].get_image(0, (50, 50)))),
+                        2: Tile(Sprite((0, 0), graphics_dict["map"].get_image(16, (50, 50)))),
+                        3: Tile(Sprite((0, 0), graphics_dict["map"].get_image(24, (50, 50)))),
+                        4: Tile(Sprite((0, 0), graphics_dict["map"].get_image(15, (50, 50)))),
+                        5: Tile(Sprite((0, 0), graphics_dict["map"].get_image(31, (50, 50)))),
+                        6: Tile(Sprite((0, 0), graphics_dict["map"].get_image(23, (50, 50)))),
+                        7: Tile(Sprite((0, 0), graphics_dict["map"].get_image(32, (50, 50)))),
+                        8: Tile(Sprite((0, 0), graphics_dict["map"].get_image(13, (50, 50))), False),
+                        9: Tile(Sprite((0, 0), graphics_dict["map"].get_image(11, (50, 50)))),
+                        10: Tile(Sprite((0, 0), graphics_dict["map"].get_image(4, (50, 50)))),
+                        11: Tile(Sprite((0, 0), graphics_dict["map"].get_image(12, (50, 50)))),
+                        12: Tile(Sprite((0, 0), graphics_dict["map"].get_image(5, (50, 50)))),
+                        13: Tile(Sprite((0, 0), graphics_dict["map"].get_image(6, (50, 50)))),
+                        14: Tile(Sprite((0, 0), graphics_dict["map"].get_image(7, (50, 50)))),
+                        15: Tile(Sprite((0, 0), graphics_dict["map"].get_image(25, (50, 50))), False),
+                        16: Tile(Sprite((0, 0), graphics_dict["map"].get_image(40, (50, 50))), False),
+                        17: Tile(Sprite((0, 0), graphics_dict["map"].get_image(33, (50, 50)), None,
+                                        (True, False)), False),
+                        18: Tile(Sprite((0, 0), graphics_dict["map"].get_image(36, (50, 50))), False),
+                        19: Tile(Sprite((0, 0), graphics_dict["map"].get_image(33, (50, 50))), False),
+                        20: Tile(Sprite((0, 0), graphics_dict["map"].get_image(36, (50, 50)), None,
+                                        (True, False)), False),
+                        21: Tile(Sprite((0, 0), graphics_dict["map"].get_image(0, (50, 50))), False),
+                        22: Tile(Sprite((0, 0), graphics_dict["map"].get_image(0, (50, 50))))
+                        }
+
+    def is_what(self, value, name):
+        if name == "TERRAIN":
+            return value == 0 or self.code["GRASS_PLAIN"] <= value <= self.code["ROCK"]
+        elif name == "STRUCTURE":
+            return self.code["CHECKERED_BASIC_1"] <= value <= self.code["WALL_BROKEN"]
+        elif name == "FLOOR":
+            return self.code["CHECKERED_BASIC_1"] <= value <= self.code["CHECKERED_GRASS_3"]
+        elif name == "WALL":
+            return self.code["WALL_LEFT_RIGHT"] <= value <= self.code["WALL_TOP_LEFT"]
+
+
